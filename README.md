b3d16580e2f2c922d07cd5ab8c25df9e5eccaab7
Joshua Bell <bjoshua@me.com>
2012-08-05 12:10:03 -0500

    Initial Commit - Cookbook skeleton
    Initial Commit, just the chef cookbook skeleton

---
+++ README.md
@@ -0,0 +1,14 @@
+Description
+===========
+Provides LWRPs for interaction with Rackspace's Cloud DNS API using Fog
+
+Requirements
+============
+rackspace-fog ruby gem
+
+Attributes
+==========
+
+Usage
+=====
+
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Joshua Bell"
+maintainer_email "bjoshua@bjoshua.me"
+license          "Apache 2.0"
+description      "Provides LWRPs for interaction with Rackspace's Cloud DNS API using Fog"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
